1b507b4b53899e63dfc8edb037d78086e6c57ced
18337713619 <3219085070@qq.com>
2025-11-16 17:51:54 +0800

    对接口用completableFuture统一封装，会出现短板效应

--- li-so-backend/src/main/java/com/limou/so/controller/SearchController.java
+++ li-so-backend/src/main/java/com/limou/so/controller/SearchController.java
@@ -25,6 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/search")
@@ -39,26 +41,43 @@ public class SearchController {
 
     //对请求进行统一的封装
     @PostMapping("/all")
-    public BaseResponse<SearchVO> searchAll(@RequestBody SearchRequest searchRequest, HttpServletRequest request) {
+    public BaseResponse<SearchVO> searchAll(@RequestBody SearchRequest searchRequest, HttpServletRequest request) throws ExecutionException, InterruptedException {
         //判断
         if (searchRequest == null) {
             return new BaseResponse<>(ErrorCode.PARAMS_ERROR);
         }
         String searchText = searchRequest.getSearchText();
         //调用不同的接口进行统一的封装 图片接口
-        Page<Picture> picturePage = pictureService.searchPicture(searchRequest);
+        CompletableFuture<Page<Picture>> pagePictureCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            //图片接口
+            return pictureService.searchPicture(searchRequest);
+        });
+//        Page<Picture> picturePage = pictureService.searchPicture(searchRequest);
 
         //用户接口
-        UserQueryRequest userQueryRequest = new UserQueryRequest();
-        userQueryRequest.setUserName(searchText);
-        BeanUtil.copyProperties(searchRequest, userQueryRequest);
-        Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-
+        CompletableFuture<Page<UserVO>> pageUserCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            UserQueryRequest userQueryRequest = new UserQueryRequest();
+            userQueryRequest.setUserName(searchText);
+            BeanUtil.copyProperties(searchRequest, userQueryRequest);
+            return userService.listUserVOByPage(userQueryRequest);
+        });
         //文章接口
-        PostQueryRequest postQueryRequest = new PostQueryRequest();
-        postQueryRequest.setSearchText(searchText);
-        BeanUtil.copyProperties(searchRequest, postQueryRequest);
-        Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest);
+//        PostQueryRequest postQueryRequest = new PostQueryRequest();
+//        postQueryRequest.setSearchText(searchText);
+//        BeanUtil.copyProperties(searchRequest, postQueryRequest);
+//        Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest);
+//
+        CompletableFuture<Page<PostVO>> pagePostCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            PostQueryRequest postQueryRequest = new PostQueryRequest();
+            postQueryRequest.setSearchText(searchText);
+            BeanUtil.copyProperties(searchRequest, postQueryRequest);
+            return postService.listPostVOByPage(postQueryRequest);
+        });
+       //等待结果
+        CompletableFuture.allOf(pageUserCompletableFuture, pagePostCompletableFuture, pagePictureCompletableFuture).join();
+        Page<Picture> picturePage = pagePictureCompletableFuture.get();
+        Page<UserVO> userVOPage = pageUserCompletableFuture.get();
+        Page<PostVO> postVOPage = pagePostCompletableFuture.get();
         //转变为 vo
         SearchVO searchVO = new SearchVO();
         searchVO.setUserVOList(userVOPage.getRecords());
